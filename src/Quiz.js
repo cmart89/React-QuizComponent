2f997b0168e629e4e3f41e691bf5526e61a8b5ab
cmart1989 <chris.martin@covered.ca.gov>
2018-08-13 21:45:17 -0700

    module:5

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,29 +5,33 @@ import QuizEnd from './QuizEnd';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { quiz_position: 1 };
-    }
-    
-    render() {
-        const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
+  constructor(props) {
+    super(props);
+    this.state = { quiz_position: 1 };
+  }
 
-        if (isQuizEnd) {
-            return (
-                <div>
-                    <QuizEnd />
-                </div>
-            )
-        }
-        else {
-            return(
-                <div>
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-                </div>
-            )
-        }
+  showNextQuestion() {
+    this.setState({ quiz_position: this.state.quiz_position + 1 });
+  }
+
+  render() {
+    const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
+
+    if (isQuizEnd) {
+      return (
+        <div>
+          <QuizEnd />
+        </div>
+      )
     }
+    else {
+      return (
+        <div>
+          <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+        </div>
+      )
+    }
+  }
 }
 
 export default Quiz;
